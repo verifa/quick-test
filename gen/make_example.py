0bd9d5e45c55658b40d2c00ed0b469272ec80327
James Elderfield <jelderfield@verifa.io>
2019-08-14 16:31:06 +0300

    Updated to have THREE files

--- gen/make_example.py
+++ gen/make_example.py
@@ -3,11 +3,32 @@ import random
 
 output_name = "../input.json"
 
+colours = [
+    ("red", 0.4),
+    ("blue", 0.4),
+    ("green", 0.1),
+    ("purple", 0.05),
+    ("orange", 0.05)
+]
+
 if __name__ == "__main__":
-    output = [{
-        "colour": "blue" if random.random() < 0.5 else "red",
-        "index": i
-    } for i in range(1000)]
+    output = []
+
+    for i in range(1000):
+        r = random.random()
+        accum = 0
+
+        for this_colour, p in colours:
+            accum += p
+
+            if r < accum:
+                colour = this_colour
+                break
+
+        output.append({
+            "colour": colour,
+            "index": i
+        })
 
     with open(output_name, "w") as out_file: 
         json.dump(output, out_file, indent=4)
